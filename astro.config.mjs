bddc0ff9c07543dcb16b6cfa3658fddc042ffae0
Suny Kim <ynux@gmx.net>
2026-08-11 15:36:22 +0200

    feat: github pages workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,3 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({});
+
+export default defineConfig({
+  site: 'https://ynux.github.io',
+  base: '/astroclass',
+})
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,20 +1,19 @@
 import { defineCollection, z } from "astro:content";
+import { file } from "astro/loaders";
 
 const projectsCollection = defineCollection({
-  type: "data", // oder 'content' falls du Markdown/MDX nutzt
+  loader: file("src/data/projects.json"),
   schema: z.object({
     title: z.string(),
-    description: z.string(),
-    tech: z.array(z.string()),
-    link: z.string().url(),
-    // Neue angeforderte Felder:
-    slug: z.string(),
-    image: z.string(),
+    fromdate: z.string(),
+    todate: z.string(),
     industry: z.string(),
-    dateRange: z.object({
-      fromDate: z.string(), // z.B. "2024-01" oder "Jan 2024"
-      toDate: z.string(), // z.B. "2025-06" oder "now"
-    }),
+    slug: z.string(),
+    description: z.string().optional(),
+    tasks: z.array(z.string()),
+    roles: z.string(),
+    skills: z.array(z.string()),
+    image: z.string().optional(),
   }),
 });
 
